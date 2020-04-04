08aab353118dc739dab3280d7e321969a1278331
Abel Kalum <chumbabel@gmail.com>
2020-04-04 20:07:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,7 +8,18 @@ class Pokemon
   end
   
   def save
-    
+   if !@id
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+   else
+      sql = "UPDATE students SET name = ? WHERE id = ?"
+      DB[:conn].execute(sql, @name, @id)
+   end
   end
   
   def find
