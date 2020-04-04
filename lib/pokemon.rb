fce27cfface25e8b43433dfa1f5350668783216d
Abel Kalum <chumbabel@gmail.com>
2020-04-04 20:00:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,8 +1,6 @@
 class Pokemon
   
-  attr_accessor :id, :name, :type, :db
-  
-  def self.initialize(id, name, type, db)
+  def self.initialize(id:, name:, type:, db:)
     @id = id
     @name = name
     @type = type
